72a08fcb6d1031f047ccbd27de4b42b3441d66fc
HIC P <nicholas.parrish@mac.com>
2024-04-12 09:10:43 -0400

    Initial Commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'test-live-reload',
-  webDir: 'www',
+  webDir: 'www/browser',
   server: {
     androidScheme: 'https'
   }
